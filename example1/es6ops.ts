813324c4962f652cf541ddf52bdb0e878d38bcfc
Pritesh Gudge <priteshgudge@gmail.com>
2019-10-04 14:52:15 +0530

    Create es6ops.ts

---
+++ example1/es6ops.ts
@@ -0,0 +1,28 @@
+
+// Exercise 1 - Maybe use an Arrow Function?
+let double = (value) => value * 2
+
+console.log(double(10));
+
+// Exercise 2 - If only we could provide some default values...
+let greet = (name: string = "Max"):void => console.log("Hello, " + name)
+greet();
+greet("Anna");
+
+// Exercise 3 - Isn't there a shorter way to get all these Values?
+const numbers = [-3, 33, 38, 5];
+console.log(Math.min(...numbers));
+
+// Exercise 4 - I have to think about Exercise 3 ...
+const newArray = [55, 20];
+newArray.push(...numbers)
+
+// Exercise 5 - That's a well-constructed array.
+const testResults = [3.89, 2.99, 1.38];
+const result1,result2,result3  = testResults;
+console.log(...testResults);
+
+// Exercise 6 - And a well-constructed object!
+const scientist = {firstName: "Will", experience: 12};
+const {firstName, _, experience} = scientist;
+console.log(firstName, experience);
